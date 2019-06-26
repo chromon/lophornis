ac28831bad4d8fcd73b707c491d21ca1aceec8f7
Ellery Zhang <allotory@msn.com>
2019-06-26 21:27:55 +0800

    style: add moment avatar

--- lib/pages/moments/moments_page.dart
+++ lib/pages/moments/moments_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:lophornis/constants/app_constants.dart';
+import 'package:lophornis/constants/app_colors.dart';
 import 'package:lophornis/widget/function_item_widget.dart';
 
 class MomentsPage extends StatefulWidget {
@@ -17,171 +18,191 @@ class _MomentsPageState extends State<MomentsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        //没有铺满也可以滑动
-        physics: AlwaysScrollableScrollPhysics(),
-        slivers: <Widget>[
-          SliverAppBar(
-            leading: IconButton(
-              icon: Icon(
-                IconData(
-                  0xe64c,
-                  fontFamily: AppConstants.IconFontFamily,
+    return Theme(
+      data: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.indigo,
+        platform: Theme.of(context).platform,
+      ),
+      child: Scaffold(
+        body: CustomScrollView(
+          //没有铺满也可以滑动
+          physics: AlwaysScrollableScrollPhysics(),
+          slivers: <Widget>[
+            SliverAppBar(
+              leading: IconButton(
+                icon: Icon(
+                  IconData(
+                    0xe64c,
+                    fontFamily: AppConstants.IconFontFamily,
+                  ),
+                  size: AppConstants.ActionIconSize + 4.0,
+                  color: Colors.white,
                 ),
-                size: AppConstants.ActionIconSize + 4.0,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            expandedHeight: _appBarHeight,
-            pinned: _appBarBehavior == AppBarBehavior.pinned,
-            floating: _appBarBehavior == AppBarBehavior.floating || _appBarBehavior == AppBarBehavior.snapping,
-            snap: _appBarBehavior == AppBarBehavior.snapping,
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.create),
-                tooltip: 'Edit',
-                onPressed: () {},
-              ),
-              PopupMenuButton<AppBarBehavior>(
-                onSelected: (AppBarBehavior value) {
-                  setState(() {
-                    _appBarBehavior = value;
-                  });
+                onPressed: () {
+                  Navigator.pop(context);
                 },
-                itemBuilder: (BuildContext context) => <PopupMenuItem<AppBarBehavior>>[
-                  const PopupMenuItem<AppBarBehavior>(
-                    value: AppBarBehavior.normal,
-                    child: Text('App bar scrolls away'),
-                  ),
-                  const PopupMenuItem<AppBarBehavior>(
-                    value: AppBarBehavior.pinned,
-                    child: Text('App bar stays put'),
-                  ),
-                  const PopupMenuItem<AppBarBehavior>(
-                    value: AppBarBehavior.floating,
-                    child: Text('App bar floats'),
-                  ),
-                  const PopupMenuItem<AppBarBehavior>(
-                    value: AppBarBehavior.snapping,
-                    child: Text('App bar snaps'),
-                  ),
-                ],
               ),
-            ],
-            flexibleSpace: FlexibleSpaceBar(
-              title: const Text('Ali Connors'),
-              background: Stack(
-                fit: StackFit.expand,
-                children: <Widget>[
-                  Image.asset(
-                    'assets/images/moments_bg.png',
-                    fit: BoxFit.cover,
-                    height: _appBarHeight,
-                  ),
-                  // This gradient ensures that the toolbar icons are distinct
-                  // against the background image.
-                  const DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment(0.0, -1.0),
-                        end: Alignment(0.0, -0.4),
-                        colors: <Color>[Color(0x60000000), Color(0x00000000)],
+              expandedHeight: _appBarHeight,
+              title: Text('朋友圈'),
+              backgroundColor: const Color(AppColors.PrimaryColor),
+              pinned: _appBarBehavior == AppBarBehavior.pinned,
+              floating: _appBarBehavior == AppBarBehavior.floating || _appBarBehavior == AppBarBehavior.snapping,
+              snap: _appBarBehavior == AppBarBehavior.snapping,
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.create),
+                  tooltip: 'Edit',
+                  onPressed: () {},
+                ),
+                PopupMenuButton<AppBarBehavior>(
+                  onSelected: (AppBarBehavior value) {
+                    setState(() {
+                      _appBarBehavior = value;
+                    });
+                  },
+                  itemBuilder: (BuildContext context) => <PopupMenuItem<AppBarBehavior>>[
+                    const PopupMenuItem<AppBarBehavior>(
+                      value: AppBarBehavior.normal,
+                      child: Text('App bar scrolls away'),
+                    ),
+                    const PopupMenuItem<AppBarBehavior>(
+                      value: AppBarBehavior.pinned,
+                      child: Text('App bar stays put'),
+                    ),
+                    const PopupMenuItem<AppBarBehavior>(
+                      value: AppBarBehavior.floating,
+                      child: Text('App bar floats'),
+                    ),
+                    const PopupMenuItem<AppBarBehavior>(
+                      value: AppBarBehavior.snapping,
+                      child: Text('App bar snaps'),
+                    ),
+                  ],
+                ),
+              ],
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text('Ellery'),
+                background: Stack(
+                  overflow: Overflow.visible,
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/moments_bg.png',
+                      fit: BoxFit.cover,
+                      height: _appBarHeight,
+                    ),
+                    Positioned(
+                      right: 14.0,
+                      bottom: -36.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(AppConstants.AvatarRadius),
+                        child: Image.asset(
+                          'assets/images/default_nor_avatar.png',
+                          width: 72.0,
+                          height: 72.0,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    
+                  ],
+                ),
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              <Widget>[
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-                FunctionItemWidget(
-                  iconPath: '',
-                  showAvatar: false,
-                  title: '账号与安全',
-                  onPressed: () {print('点击了账号与安全');},
-                ),
-              ]
+            SliverList(
+              delegate: SliverChildListDelegate(
+                <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 36.0),
+                    child: Column(
+                      children: <Widget>[
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+                        FunctionItemWidget(
+                          iconPath: '',
+                          showAvatar: false,
+                          title: '账号与安全',
+                          onPressed: () {print('点击了账号与安全');},
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ]
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
